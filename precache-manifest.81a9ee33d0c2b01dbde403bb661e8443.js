858fa869a368db5b7daf3df593941d39618ee50b
Brandon Tsui <itstsui@gmail.com>
2020-05-19 23:19:05 -0400

    Updates

---
+++ precache-manifest.81a9ee33d0c2b01dbde403bb661e8443.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "1241d26c04721c82c955",
+    "url": "/static/css/main.74ae560e.chunk.css"
+  },
+  {
+    "revision": "1241d26c04721c82c955",
+    "url": "/static/js/main.1241d26c.chunk.js"
+  },
+  {
+    "revision": "6eb6405c9d99258624bc",
+    "url": "/static/js/1.6eb6405c.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "cdc76566d524a48689847ad5bb87eec4",
+    "url": "/index.html"
+  }
+];
