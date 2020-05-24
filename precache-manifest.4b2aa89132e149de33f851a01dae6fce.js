d1184fa032f9495c8ea03cfef57ef9b7b2137437
Brandon Tsui <itstsui@gmail.com>
2020-05-23 22:52:45 -0400

    Updates

---
+++ precache-manifest.4b2aa89132e149de33f851a01dae6fce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "07f03de760e5c718a97a13c157103910",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a94b8ea967de82c3029b",
+    "url": "/static/css/2.8f118048.chunk.css"
+  },
+  {
+    "revision": "9e7ab2a011e4fa89facc",
+    "url": "/static/css/main.102959d5.chunk.css"
+  },
+  {
+    "revision": "a94b8ea967de82c3029b",
+    "url": "/static/js/2.ea01347b.chunk.js"
+  },
+  {
+    "revision": "7bc9b478038c6bedf8340d67bd512bd3",
+    "url": "/static/js/2.ea01347b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e7ab2a011e4fa89facc",
+    "url": "/static/js/main.94f0fe9e.chunk.js"
+  },
+  {
+    "revision": "80e274ec0ec388e5e202",
+    "url": "/static/js/runtime-main.c8b434d9.js"
+  }
+]);
